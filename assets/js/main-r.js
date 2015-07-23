fc2191f02f3a5d07f0a6a926c880526bf6ad340f
Valera Rozuvan <valera.rozuvan@gmail.com>
2015-07-24 01:51:42 +0300

    Adding RequireJS.

--- assets/js/main-r.js
+++ assets/js/main-r.js
@@ -1,1 +1,22 @@
 console.log('[DEBUG]: We are in "main-r.js" file.');
+
+require.config({
+  paths: {
+    Modernizr: 'vendor/modernizr-2.6.2.custom.min'
+  },
+  shim: {
+    Modernizr: {
+      exports: 'Modernizr'
+    }
+  }
+});
+
+require(['Modernizr'], function (Modernizr) {
+  console.log('[DEBUG]: We are in first require() callback function.');
+
+  // For Example.
+  if (Modernizr.boxshadow) {
+    // Do something here.
+    console.log('[DEBUG]: Modernizr.boxshadow is truthy.');
+  }
+});
