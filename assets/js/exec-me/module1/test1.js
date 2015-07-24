0bb07d751d4436983be0b39ef31f7200b897ea00
Valera Rozuvan <valera.rozuvan@gmail.com>
2015-07-24 03:45:04 +0300

    Adding RequireJS.

--- assets/js/exec-me/module1/test1.js
+++ assets/js/exec-me/module1/test1.js
@@ -1,53 +1,51 @@
-define('module1-test1', ['jQuery'], function ($) {
-  var el, cEl, hasRun = false;
+(function (define, require, undefined) {
+  'use strict';
 
-  console.log('[DEBUG]: We are in define("module1-test1") function.');
+  define('module1-test1', ['jQuery'], function ($) {
+    var el, cEl, hasRun = false;
 
-  function init(_el) {
-    console.log('[DEBUG]: We are in init() function.');
+    function init(_el) {
+      el = _el;
 
-    el = _el;
+      createCanvas();
 
-    createCanvas();
+      $(el).find('a.btn-run-js').click(function (event) {
+        event.preventDefault();
+        event.stopPropagation();
 
-    $(el).find('a.btn-run-js').click(function (event) {
-      event.preventDefault();
-      event.stopPropagation();
+        if (hasRun === false) {
+          hasRun = true;
+          run();
+        }
+      });
+    }
 
-      if (hasRun === false) {
-        hasRun = true;
-        run();
-      }
-    });
-  }
+    function createCanvas() {
+      cEl = $('<canvas id="output-canvas-1"></canvas>');
 
-  function createCanvas() {
-    cEl = $('<canvas id="output-canvas-1"></canvas>');
+      cEl.css({
+        width: '400px',
+        height: '200px',
+        display: 'inline-block',
+        'background-color': '#ffffff',
+        margin: '2%'
+      });
 
-    cEl.css({
-      width: '400px',
-      height: '200px',
-      display: 'inline-block',
-      'background-color': '#ffffff',
-      margin: '2%'
-    });
+      $(el).append(cEl);
+    }
 
-    $(el).append(cEl);
-  }
+    function run() {
+      var canvas, ctx;
 
-  function run() {
-    var canvas, ctx;
+      canvas = cEl.get(0);
+      ctx = canvas.getContext('2d');
 
-    console.log('[DEBUG]: We are in run() function.');
+      ctx.fillStyle = 'black';
+      ctx.fillRect(10, 10, 20, 20);
+    }
 
-    canvas = cEl.get(0);
-    ctx = canvas.getContext('2d');
-
-    ctx.fillStyle = 'black';
-    ctx.fillRect(10, 10, 20, 20);
-  }
-
-  return {
-    init: init
-  };
-});
+    return {
+      init: init
+    };
+  });
+}(window.define, window.require));
--- assets/js/main-r.js
+++ assets/js/main-r.js
@@ -1,54 +1,38 @@
-console.log('[DEBUG]: We are in "main-r.js" file.');
+(function (define, require, undefined) {
+  'use strict';
 
-require.config({
-  paths: {
-    Modernizr: 'vendor/modernizr-2.8.3.custom.min',
-    jQuery: 'vendor/jquery-1.9.1.min',
-    scripts: 'scripts.min',
-    'module1-test1': 'exec-me/module1/test1'
-  },
-  shim: {
-    Modernizr: {
-      exports: 'Modernizr'
+  require.config({
+    paths: {
+      Modernizr: 'vendor/modernizr-2.8.3.custom.min',
+      jQuery: 'vendor/jquery-1.9.1.min',
+      scripts: 'scripts.min',
+      'module1-test1': 'exec-me/module1/test1'
     },
-    scripts: {
-      exports: '_main_loaded_',
-      deps: [
-        'jQuery', 'Modernizr'
-      ]
-    },
-    jQuery: {
-      exports: 'jQuery'
+    shim: {
+      Modernizr: {
+        exports: 'Modernizr'
+      },
+      scripts: {
+        exports: '_main_loaded_',
+        deps: [
+          'jQuery', 'Modernizr'
+        ]
+      },
+      jQuery: {
+        exports: 'jQuery'
+      }
     }
-  }
-});
+  });
 
-require(['Modernizr', 'jQuery', 'scripts'], function (Modernizr, $) {
-  console.log('[DEBUG]: We are in first require() callback function.');
+  require(['Modernizr', 'jQuery', 'scripts'], function (Modernizr, $) {
+    $(document).ready(function () {
+      $('.exec-me').each(function (index, el) {
+        var jsSource = $(el).data('js-source');
 
-  $(document).ready(function () {
-    console.log('[DEBUG]: We are in $(document).ready() callback function.');
-
-    // For Example.
-    if (Modernizr.canvas) {
-      // Do something here.
-      console.log('[DEBUG]: Modernizr.canvas is truthy.');
-    } else {
-      console.log('[DEBUG]: Modernizr.canvas is falsy.');
-      console.log('---> Modernizr = ', Modernizr);
-    }
-
-    console.log('');
-
-    $('.exec-me').each(function (index, el) {
-      var jsSource = $(el).data('js-source');
-
-      console.log('[DEBUG]: Found an "exec-me" element.');
-      console.log('---> data["js-source"] = "' + jsSource + '".');
-
-      require([jsSource], function (module) {
-        module.init(el);
+        require([jsSource], function (module) {
+          module.init(el);
+        });
       });
     });
   });
-});
+}(window.define, window.require));
