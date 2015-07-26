8c7d1e1339c087d3b90280a46c63f970bc530576
Valera Rozuvan <valera.rozuvan@gmail.com>
2015-07-26 21:27:16 +0300

    Added a general build all gulp task.

--- site/assets/gulpfile.js
+++ site/assets/gulpfile.js
@@ -3,6 +3,10 @@ var gulp = require('gulp'),
   concat = require('gulp-concat'),
   rename = require('gulp-rename'),
 
+  gutil = require('gulp-util'),
+
+  shell = require('gulp-shell'),
+
   jscs = require('gulp-jscs'),
   jshint = require('gulp-jshint'),
 
@@ -26,53 +30,110 @@ var gulp = require('gulp'),
   CONCAT_JS = 'scripts.min.js',
   JS_OUT_DIR = './js';
 
-gulp.task('concat', function() {
-  return gulp.src(projectJsFiles)
+gulp.task('concat', function (cb) {
+  gutil.log(gutil.colors.cyan('===== START OF: concat task ======'));
+
+  gulp.src(projectJsFiles)
     .pipe(concat(CONCAT_JS))
-    .pipe(gulp.dest(JS_OUT_DIR));
+    .pipe(gulp.dest(JS_OUT_DIR))
+    .on('end', function () {
+      gutil.log(gutil.colors.cyan('===== END OF: concat task ======'));
+      cb();
+    });
 });
 
-gulp.task('compress', function() {
-  return gulp.src(JS_OUT_DIR + '/' + CONCAT_JS)
+gulp.task('compress', ['concat'], function (cb) {
+  gutil.log(gutil.colors.cyan('===== START OF: compress task ======'));
+
+  gulp.src(JS_OUT_DIR + '/' + CONCAT_JS)
     .pipe(uglify())
-    .pipe(gulp.dest(JS_OUT_DIR));
+    .pipe(gulp.dest(JS_OUT_DIR))
+    .on('end', function () {
+      gutil.log(gutil.colors.cyan('===== END OF: compress task ======'));
+      cb();
+    });
 });
+
+gulp.task('build-js', ['compress']);
 
 gulp.task('style', function () {
+  gutil.log(gutil.colors.cyan('===== START OF: style task ======'));
+
   return gulp.src([
     './js/**/*.js',
     '!./js/vendor/*.js',
     '!./js/plugins/*.js',
     '!./js/**/*.min.js'
   ])
-  .pipe(jscs({
-    fix: false,
-    verbose: true
-  }));
+    .pipe(jscs({
+      fix: false,
+      verbose: true
+    }))
+    .on('end', function () {
+      gutil.log(gutil.colors.cyan('===== END OF: style task ======'));
+    });
 });
 
 gulp.task('hint', function () {
+  gutil.log(gutil.colors.cyan('===== START OF: hint task ======'));
+
   return gulp.src([
     './js/**/*.js',
     '!./js/vendor/*.js',
     '!./js/plugins/*.js',
     '!./js/**/*.min.js'
   ])
-  .pipe(jshint())
-  .pipe(jshint.reporter('default'));
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'))
+    .on('end', function () {
+      gutil.log(gutil.colors.cyan('===== END OF: hint task ======'));
+    });
 });
 
-gulp.task('less', function () {
-  return gulp.src('./less/main.less')
+gulp.task('check', ['style', 'hint']);
+
+gulp.task('less', function (cb) {
+  gutil.log(gutil.colors.cyan('===== START OF: less task ======'));
+
+  gulp.src('./less/main.less')
     .pipe(less({
       paths: [ path.join(__dirname, 'less', 'includes') ]
     }))
-    .pipe(gulp.dest('./css'));
+    .pipe(gulp.dest('./css'))
+    .on('end', function () {
+      gutil.log(gutil.colors.cyan('===== END OF: less task ======'));
+      cb();
+    });
 });
 
-gulp.task('minify-css', function() {
-  return gulp.src('./css/main.css')
+gulp.task('minify-css', ['less'], function (cb) {
+  gutil.log(gutil.colors.cyan('===== START OF: minify-css task ======'));
+
+  gulp.src('./css/main.css')
     .pipe(minifyCss())
     .pipe(rename('main.min.css'))
-    .pipe(gulp.dest('./css'));
+    .pipe(gulp.dest('./css'))
+    .on('end', function () {
+      gutil.log(gutil.colors.cyan('===== END OF: minify-css task ======'));
+      cb();
+    });
 });
+
+gulp.task('build-css', ['minify-css']);
+
+gulp.task('jekyll', ['build-js', 'build-css', 'check'], function (cb) {
+  gutil.log(gutil.colors.cyan('===== START OF: jekyll task ======'));
+
+  gulp.src('')
+    .pipe(shell([
+      'jekyll build ' +
+        '--source ../../site ' +
+        '--destination ../../../gh-pages'
+    ]))
+    .on('end', function () {
+      gutil.log(gutil.colors.cyan('===== END OF: jekyll task ======'));
+      cb();
+    });
+});
+
+gulp.task('build', ['jekyll']);
