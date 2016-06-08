808a0149bbbdd89c159c295deb17eabf8a78438b
Valera Rozuvan <valera.rozuvan@gmail.com>
2016-06-08 17:32:49 +0300

    Updated Google analytics tracking code.

--- assets/js/google-analytics.js
+++ assets/js/google-analytics.js
@@ -1,25 +1,16 @@
 (function (define, require, undefined) {
-  'use strict';
+    'use strict';
 
-  define('google-analytics', [], function () {
-    function init() {
-      var _gaq = _gaq || [];
+    define('google-analytics', [], function () {
+        function init() {
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-      _gaq.push(['_setAccount', 'UA-49332155-1']);
-      _gaq.push(['_trackPageview']);
-
-      (function () {
-        var ga, s;
-
-        ga = document.createElement('script');
-        ga.type = 'text/javascript';
-        ga.async = true;
-        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-
-        s = document.getElementsByTagName('script')[0];
-        s.parentNode.insertBefore(ga, s);
-      }());
-    }
+            ga('create', 'UA-49332155-1', 'auto');
+            ga('send', 'pageview');
+        }
 
     return {
       init: init
